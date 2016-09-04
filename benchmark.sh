971ab24e655e998cbf0c2f665b3790149b1dd805
Alex Bertram <alex@bedatadriven.com>
2016-09-04 16:42:12 +0200

    Removed warmup time.
    Our benchmark now iterators over 10, 20, 30 etc users as part of
    the overall benchmark sequence, so we don't need a warmup for each
    phase.

--- benchmark.sh
+++ benchmark.sh
@@ -9,20 +9,28 @@ mkdir -p results
 # Benchmark the performance as we scale the machine
 # size vertically from 4 cores to 16
 
-for cores in 4 8 16; do
-
-    # Ensure our environment is up and running with
-    # the right machine type 
-    terraform apply -var machine_type=n1-standard-$cores gcp
-
-    # Benchmark OpenCPU and Renjin against successive number of concurrent users
-
-    for users in 10 50 100 200 500; do
-       jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
-       jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/opencpu-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
-    done
+# n1-standard-4
+cores=4
+terraform apply -var machine_type=n1-standard-$cores gcp
+    
+for users in 10 50 75 100 125 150; do
+   jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
 done
 
-# Cleanup the environment
+for users in 10 50 75 100 125 150; do
+   jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
+done
 
-terraform destroy gcp
+# n1-standard-8
+cores=8
+terraform apply -var machine_type=n1-standard-$cores gcp
+    
+for users in 10 50 100 200 300 400 500; do
+   jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
+done
+
+for users in 10 50 100 200 300 400 500; do
+   jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
+done
+
+
