15acf230fa542eccee8e82071be9773f08f9ef6f
Alex Bertram <alex@bedatadriven.com>
2016-09-08 11:02:50 +0200

    Full benchmarking runs
    To save time, only run high number of concurrent users (>500) on
    8+ core configuration.

--- benchmark.sh
+++ benchmark.sh
@@ -14,23 +14,41 @@ cores=4
 terraform apply -var machine_type=n1-standard-$cores gcp
     
 for users in 10 50 75 100 125 150; do
+   rm -f results/renjin-${cores}-${users}.csv
    jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
 done
 
 for users in 10 50 75 100 125 150; do
-   jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
-done
+   rm -f results/opencpu-${cores}-${users}.csv
+   jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/opencpu-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
+#done
 
 # n1-standard-8
 cores=8
 terraform apply -var machine_type=n1-standard-$cores gcp
     
 for users in 10 50 100 200 300 400 500; do
-   jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
+  rm -f results/renjin-${cores}-${users}.csv
+  jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
 done
 
 for users in 10 50 100 200 300 400 500; do
-   jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
+  rm -f results/opencpu-${cores}-${users}.csv
+  jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/opencpu-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
+done
+
+# n1-standard-16
+cores=16
+terraform apply -var machine_type=n1-standard-$cores gcp
+    
+for users in 10 50 100 200 300 400 500 800 1000; do
+  rm -f results/renjin-${cores}-${users}.csv
+  jmeter -Jhost=`terraform output renjin_ip` -Jusers=$users -Joutput="results/renjin-${cores}-${users}.csv" -n -t jmeter/Renjin-Fraud.jmx 
+done
+
+for users in 10 50 100 200 300 400 500; do
+  rm -f results/opencpu-${cores}-${users}.csv
+  jmeter -Jhost=`terraform output opencpu_ip` -Jusers=$users -Joutput="results/opencpu-${cores}-${users}.csv" -n -t jmeter/OpenCPU-Fraud.jmx 
 done
 
 
