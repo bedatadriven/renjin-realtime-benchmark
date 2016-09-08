210e1701b1573ac2a713828826e3d15642760e85
Alex Bertram <alex@bedatadriven.com>
2016-09-08 10:46:20 +0200

    Added Renjin to build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@
 # Package the fraudscore package as tar.gz
 (cd application; R CMD build fraudscore)
 
-
+# Invoke Apache Maven to build a .deb package for the service
+# that we can deploy directly.
+(cd application/renjin-service; mvn clean package)
